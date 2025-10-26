4d73086885a50c35faf74648db13b0fe52d723cf
ju4ndrc <jdramirez90@ucatolica.edu.co>
2025-10-26 18:33:08 -0500

    [+]book & user models was created

--- models.py
+++ models.py
@@ -14,18 +14,49 @@ class UserBase(SQLModel):
     name:str | None = Field(description="User name")
     year : int | None = Field(description="User year")
     rol: Rol | None = Field(description="Rol", default=Rol.READER)
-    is_active: bool | None = Field(description="Active User", default=True)
 
 
 class User(UserBase, table=True):
     id:uuid.UUID = Field(default_factory=uuid.uuid4,primary_key=True)
+    is_active: bool | None = Field(description="Active User", default=True)
 
+    lends: list["Lend"] = Relationship(back_populates="user")
 
 class Library(LibraryBase, table=True):
     id:uuid.UUID = Field(default_factory=uuid.uuid4,primary_key=True)
     date:datetime.datetime | None = Field(default_factory=datetime.datetime.now)
 
-class
+class BookBase(SQLModel):
+    title: str = Field(description="Book title")
+    isbn: str = Field(description="Unique ISBN")
+    year: int = Field(description="Publication year")
+    copies: int = Field(default=1, description="Number of copies available")
+
+class Book(BookBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4,primary_key=True)
+    is_active: bool | None = Field(description="Active Book", default=True)
+
+    lends : list["Lend"]Relationship(back_populates="book")
+class LendBookBase(SQLModel):
+    loan_date: datetime.date = Field(default_factory=datetime.date.today)
+    return_date: datetime.date | None = None
+    fine: float | None = Field(default=0.0, description="Fine for late return")
+
+
+class Lend(LendBookBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    user_id: uuid.UUID = Field(foreign_key="user.id")#relacion entre clase lend y clase user
+    book_id: uuid.UUID = Field(foreign_key="book.id")#relacion entre clase book y clase prestamo
+
+    user:"User" = Relationship(back_populates="lends")
+    book:"Book" = Relationship(back_populates="lends")
+class CreateBook(UserBase):
+    pass
+
+class UpdateBook(LibraryBase):
+    pass
+
+
 
 class CreateUser(UserBase):
     pass
