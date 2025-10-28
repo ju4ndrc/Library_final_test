a46e21b65ecdcdeaebc542d2151d3b86c8ce48ef
ju4ndrc <jdramirez90@ucatolica.edu.co>
2025-10-28 01:20:05 -0500

    [+]Routes added

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from .auth import auth_router
-from .routers import library, users
+from .routers import library, users, authors, book
 from db import create_tables
 
 app = FastAPI(lifespan=create_tables,title="Library",version="0.0.1")
@@ -8,6 +8,8 @@ app = FastAPI(lifespan=create_tables,title="Library",version="0.0.1")
 app.include_router(library.router)
 app.include_router(auth_router.router)
 app.include_router(users.router)
+app.include_router(authors.router)
+app.include_router(book.router)
 @app.get("/")
 async def root():
     return{"Hello":"world"}
