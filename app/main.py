592f668803546cec00e75295f2c99f956d758eaa
ju4ndrc <jdramirez90@ucatolica.edu.co>
2025-10-28 19:24:33 -0500

    [+]Incorporating into the main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from .auth import auth_router
-from .routers import library, users, authors, book
+from .routers import library, users, authors, book, lends
 from db import create_tables
 
 app = FastAPI(lifespan=create_tables,title="Library",version="0.0.1")
@@ -10,6 +10,7 @@ app.include_router(auth_router.router)
 app.include_router(users.router)
 app.include_router(authors.router)
 app.include_router(book.router)
+app.include_router(lends.router)
 @app.get("/")
 async def root():
     return{"Hello":"world"}
